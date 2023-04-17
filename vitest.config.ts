31c1b298f4451b6f40e1c46b255e6951ec342062
Geoff Crew <gc97@cornell.edu>
2023-04-17 17:14:35 -0400

    remove caches from test watch

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     include: ['specs/*.spec.js'],
-    watchExclude: ['node_modules/**', 'solve/**']
+    watchExclude: ['node_modules/**', 'solve/**', 'answerscache/**', 'processedcache/**']
   },
 })
