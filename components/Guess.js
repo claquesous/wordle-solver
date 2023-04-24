32f8247f2d897790ed5e2d184c142140b6d55dff
Geoff Crew <gc97@cornell.edu>
2023-04-23 22:09:07 -0400

    add next frontend

---
+++ components/Guess.js
@@ -0,0 +1,12 @@
+export default function Guess() {
+  return (
+    <div>
+      <input type="text" maxLength="1" />
+      <input type="text" maxLength="1" />
+      <input type="text" maxLength="1" />
+      <input type="text" maxLength="1" />
+      <input type="text" maxLength="1" />
+    </div>
+  );
+};
+
---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Guess from '../components/Guess.js'
+
+function HomePage() {
+  return <div>
+	<Guess />
+	<Guess />
+	<Guess />
+	<Guess />
+	<Guess />
+	<Guess />
+  </div>
+}
+
+export default HomePage
+
