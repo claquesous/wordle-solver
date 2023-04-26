26daa6f6480c2336c62d7caf76a2321c87c3e877
Geoff Crew <gc97@cornell.edu>
2023-04-26 00:16:26 -0400

    begin saving word state

--- components/Letter.js
+++ components/Letter.js
@@ -1,6 +1,38 @@
-export default function Letter() {
+import { useState, useRef, useEffect } from 'react'
+
+export default function Letter({ position, onSetLetter, cursor }) {
+  const [value, setValue] = useState('')
+  const [outcome, setOutcome] = useState(0)
+
+  const cursorReference = useRef(null)
+
+  useEffect(() => {
+    if (position === cursor) {
+      cursorReference.current.focus()
+    }
+  }, [cursor, position])
+
+  function handlePress({ nativeEvent }) {
+    const key = nativeEvent.data
+    if (key?.match(/[a-zA-Z]/)) {
+      setValue(key.toUpperCase())
+      onSetLetter(position, key.toLowerCase())
+    }
+    else {
+      setValue(value)
+    }
+  }
+
+  function cycleOutcome() {
+    setOutcome((outcome+1)%3)
+  }
+
   return (
-      <input type="text" maxLength="1" />
-  );
-};
+      <input type="text" maxLength="1" value={value}
+        onChange={handlePress} onClick={cycleOutcome}
+	ref={cursorReference}
+	pattern="[a-z]"
+      />
+  )
+}
 
--- components/Word.js
+++ components/Word.js
@@ -1,14 +1,24 @@
+import { useState } from 'react'
 import Letter from './Letter.js'
 
 export default function Word() {
+  const [cursor, setCursor] = useState(0)
+
+  let guess = []
+
+  function handleLetter(position, letter) {
+    guess[position] = letter
+    setCursor(position+1)
+  }
+
   return (
-    <div>
-      <Letter />
-      <Letter />
-      <Letter />
-      <Letter />
-      <Letter />
+    <div onKeyPress={handleLetter}>
+      <Letter position={ 0 } onSetLetter={ handleLetter } cursor={ cursor } />
+      <Letter position={ 1 } onSetLetter={ handleLetter } cursor={ cursor } />
+      <Letter position={ 2 } onSetLetter={ handleLetter } cursor={ cursor } />
+      <Letter position={ 3 } onSetLetter={ handleLetter } cursor={ cursor } />
+      <Letter position={ 4 } onSetLetter={ handleLetter } cursor={ cursor } />
     </div>
-  );
-};
+  )
+}
 
